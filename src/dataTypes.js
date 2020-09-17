2f60f1c916eac85efe68f1d12592475e08c2d970
Morgan <c5ipo7i@yahoo.com>
2020-09-17 16:25:10 -0700

    feat: added Position, Action, Blind class. Updated other datatypes to account for zero padding

--- src/dataTypes.js
+++ src/dataTypes.js
@@ -1,6 +1,7 @@
 
-export const positionDict = { 0: "SB", 1: "BB", 2: "dealer" };
-export const actionDict = {
+export const positionDict = { 1: "SB", 2: "BB", 3: "dealer" };
+export const Position = { "SB" : 1, "BB":2, "dealer":3 };
+export const actionIndex = {
     0: "check",
     1: "fold",
     2: "call",
@@ -8,5 +9,38 @@ export const actionDict = {
     4: "raise",
     5: "unopened"
 };
-export const streetStart = { 0: "SB", 1: "BB", 2: "BB", 3: "BB" };
-export const streetDict = {0:'preflop',1:'flop',2:'turn',3:'river'}
+export const actionDict = {
+    1: "check",
+    2: "fold",
+    3: "call",
+    4: "bet",
+    5: "raise",
+    6: "unopened"
+};
+// With zero padding, actions are offset by 1
+export const actionOffset = {
+    1: "check",
+    2: "fold",
+    3: "call",
+    4: "bet",
+    5: "raise",
+    6: "unopened"
+};
+export const Action = {
+    "check" :1,
+    "fold" :2,
+    "call" :3,
+    "bet" :4,
+    "raise" :5,
+    "unopened" :6,
+    "offset" : 1
+};
+// With zero padding, blinds are offset by 1
+export const Blind = {
+    'POSTED' : 2,
+    'NO_BLIND' : 1,
+    'PADDING' : 0
+}
+// With zero padding, Streets are offset by 1
+export const streetStart = { 1: "SB", 2: "BB", 3: "BB", 4: "BB" };
+export const streetDict = {1:'preflop',2:'flop',3:'turn',4:'river'}
